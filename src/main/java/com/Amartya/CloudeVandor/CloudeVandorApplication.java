5ba33371f48ba68e07099bcbc836f7b87647e516
Amartya-007 <amartya1820@gmail.com>
2024-10-24 16:24:38 +0530

    Initial commit

---
+++ src/main/java/com/Amartya/CloudeVandor/CloudeVandorApplication.java
@@ -0,0 +1,13 @@
+package com.Amartya.CloudeVandor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudeVandorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudeVandorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Amartya/CloudeVandor/CloudeVandorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Amartya.CloudeVandor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudeVandorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
